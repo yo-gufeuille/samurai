2d107f7735e8cbc86e19602ebc8abe380b49eb38
yo-gufeuille <enri050921@gmail.com>
2025-02-12 17:44:13 +0900

    javascript-basic-kadai

---
+++ kadai_021/event.js
@@ -0,0 +1,15 @@
+  const btn = document.getElementById('btn');
+
+  const text = document.getElementById('text');
+ 
+  btn.addEventListener('click', () => {
+
+  const childList = document.createElement('li');
+
+  childList.textContent = 'ボタンをクリックしました';
+
+  setTimeout(() => {
+    text.textContent    = "ボタンをクリックしました。";
+  },2000);
+
+});
